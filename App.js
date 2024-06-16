6e65b31b5bae15900b2aa561f12ef6960a7d2944
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 17:53:53 +0000

    Invalid DateTime

--- App.js
+++ App.js
@@ -33,14 +33,14 @@ export default function App() {
           selectedImage={selectedImage}
         />
       </View>
-      <View style={styles.footerContainer}>
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
-        />
-        <Button label="Use this photo" onPress={ () => setShowAppOptions(true)} />
-      </View>
+      {showAppOptions ? (
+        <View />
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
      
      
       <StatusBar style="auto" />
