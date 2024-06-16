306bdc88a016541d08874a5d8a89d91362fb73ad
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 17:34:05 +0000

    Invalid DateTime

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -2,9 +2,7 @@ import { StyleSheet, Image } from 'react-native';
 
 export default function ImageViewer({ placeholderImageSource, selectedImage }) {
    const imageSource = selectedImage ? {uri: selectedImage} : placeholderImageSource;
-    return (
-    <Image source={imageSource} style={styles.image} />
-  );
+    return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
