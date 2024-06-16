84843d8993c40f01fd6a839f144f1164500761b9
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 17:00:16 +0000

    Invalid DateTime

--- components/Button.js
+++ components/Button.js
@@ -11,3 +11,29 @@ export default function Button ({ label }){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+      width: 320,
+      height: 68,
+      marginHorizontal: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 3,
+    },
+    button: {
+      borderRadius: 10,
+      width: '100%',
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row',
+    },
+    buttonIcon: {
+      paddingRight: 8,
+    },
+    buttonLabel: {
+      color: '#fff',
+      fontSize: 16,
+    },
+  });
