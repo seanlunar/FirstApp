5ce468fd767584c10085a1b08923720a7c42dd33
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 17:58:55 +0000

    Invalid DateTime

--- App.js
+++ App.js
@@ -25,6 +25,18 @@ export default function App() {
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -36,10 +48,13 @@ export default function App() {
       {showAppOptions ? (
         <View />
       ) : (
-        <View style={styles.footerContainer}>
-          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        <View style={styles.optionsContainer}>
+        <View style={styles.optionsRow}>
+          <IconButton icon="refresh" label="Reset" onPress={onReset} />
+          <CircleButton onPress={onAddSticker} />
+          <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
         </View>
+      </View>
       )}
      
      
