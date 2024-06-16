5d9210f6a32e98c11c54eb40216a010479cb5c9b
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 16:43:14 +0000

    Invalid DateTime

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImage  = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
