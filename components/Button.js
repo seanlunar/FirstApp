636c6736cae1649f91d36ee7de80aeff38afeac0
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 17:12:37 +0000

    Invalid DateTime

--- components/Button.js
+++ components/Button.js
@@ -1,15 +1,20 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
 export default function Button ({ label }){
-    return (
-        <View style={StyleSheet.buttonContainer}>
-            <Pressable style={StyleSheet.button} onPress={() => alert(' you have pressed a button')}>
-                <Text style={StyleSheet.buttonLabel}>
-                    {label}
-                </Text>
-            </Pressable>
-        </View>
-    )
+    if (theme === "primary"){
+        return (
+            <View style={StyleSheet.buttonContainer}>
+                <Pressable style={StyleSheet.button} onPress={() => alert(' you have pressed a button')}>
+                    <Text style={StyleSheet.buttonLabel}>
+                        {label}
+                    </Text>
+                </Pressable>
+            </View>
+        )
+    }
+  
 }
 
 const styles = StyleSheet.create({
