ab346b8bc1ab8cfaeaaa4f8d8a0b610adfb902c5
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 16:48:29 +0000

    Invalid DateTime

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const PlaceholderImage  = require('./assets/images/background-image.png')
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={stykes.imageContainer}>
+      <View style={styles.imageContainer}>
         <Image source={PlaceholderImage} style={styles.image}/>
       </View>
       <StatusBar style="auto" />
@@ -17,8 +17,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'peru',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
