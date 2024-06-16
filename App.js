0ce9a06e47ef895ecc25c5f4b2adad69c61f0cb0
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 17:36:22 +0000

    Invalid DateTime

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-import Button from  './components/Button';
+import Button from "./components/Button";
 
-import ImageViewer from './components/ImageViewer';
-import * as ImagePicker from 'expo-image-picker';
-import { useState}  from 'react';
+import ImageViewer from "./components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
-
-const PlaceholderImage = require('./assets/images/background-image.png');
+const PlaceholderImage = require("./assets/images/background-image.png");
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const pickImageAsync = async () => {
@@ -17,24 +16,28 @@ export default function App() {
       quality: 1,
     });
 
-    if(!result.canceled){
+    if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
     } else {
-      alert(' you did not select any image.')
+      alert(" you did not select any image.");
     }
-  }
-
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-       <ImageViewer placeholderImageSource={PlaceholderImage}/>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo"/>
-
-
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -44,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
@@ -56,8 +59,8 @@ const styles = StyleSheet.create({
     height: 440,
     borderRadius: 18,
   },
-  footerContainer:{
-    flex: 1/3,
-    alignItems: 'center',
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
