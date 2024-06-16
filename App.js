fd335f8cc84a9c7beeee57322de92b8e485c61a4
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-06-16 16:37:49 +0000

    Invalid DateTime

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>i have started coding</Text>
+      <Text style={{ color:'red' }} >i have started coding</Text>
       <StatusBar style="auto" />
     </View>
   );
